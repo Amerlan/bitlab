a4d379e9af00947070184b6d9c1bff18f9af6191
Amerlan <amerlan-t@bk.ru>
2021-10-08 14:51:10 +0600

    создал пользователя

--- shop/urls.py
+++ shop/urls.py
@@ -3,11 +3,12 @@ from .views import \
     load_page_create_product,\
     create_product,\
     login_view,\
-    logout_view
+    logout_view, show_users
 
 shop_urlpatterns = [
     path('product/', load_page_create_product),
     path('create-product/', create_product, name="creation"),
     path('login/', login_view, name='login'),
-    path('logout/', logout_view, name='logout')
+    path('logout/', logout_view, name='logout'),
+    path('show_users/', show_users),
 ]
