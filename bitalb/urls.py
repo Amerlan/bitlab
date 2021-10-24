a49abf3a2982aadfaaa44a563741c4216fa99ea4
Amerlan <amerlan-t@bk.ru>
2021-10-24 21:41:05 +0600

    Lesson 4. Swagger

--- bitalb/urls.py
+++ bitalb/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from shop.urls import shop_urlpatterns
+from .swagger import urlpatterns as swagger_urlpatterns
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -9,6 +10,7 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(swagger_urlpatterns)),
     path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
